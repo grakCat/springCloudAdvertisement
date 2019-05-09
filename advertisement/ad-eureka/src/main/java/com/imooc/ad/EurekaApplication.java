6e06d031713659e2b4ed4441742909c7e5149925
GGhouyong <591471019@qq.com>
2019-05-09 16:49:37 +0800

    no message

--- advertisement/ad-eureka/src/main/java/com/imooc/ad/EurekaApplication.java
+++ advertisement/ad-eureka/src/main/java/com/imooc/ad/EurekaApplication.java
@@ -2,6 +2,7 @@ package com.imooc.ad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Created on 2019/5/9.
