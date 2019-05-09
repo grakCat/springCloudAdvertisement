bf335a3371378f5c1a214ee4a6e5207910ae3e06
HouYong <591471019@qq.com>
2019-05-09 15:01:58 +0800

    no message

--- advertisement/ad-eureka/src/main/java/com/imooc/ad/EurekaApplication.java
+++ advertisement/ad-eureka/src/main/java/com/imooc/ad/EurekaApplication.java
@@ -1,13 +1,19 @@
-package com.stude.advertisement;
+package com.imooc.ad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Created on 2019/5/9.
+ *
+ * @author hy
+ * @since 1.0
+ */
+@EnableEurekaServer
 @SpringBootApplication
-public class AdvertisementApplication {
+public class EurekaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AdvertisementApplication.class, args);
+        SpringApplication.run(EurekaApplication.class,args);
     }
-
 }
