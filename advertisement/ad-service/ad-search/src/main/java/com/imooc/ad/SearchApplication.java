c9fa37d885b957fb985aedd14049affd9ffdcf0b
HouYong <591471019@qq.com>
2019-05-15 17:44:36 +0800

    no message

---
+++ advertisement/ad-service/ad-search/src/main/java/com/imooc/ad/SearchApplication.java
@@ -0,0 +1,36 @@
+package com.imooc.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by Qinyi.
+ */
+@EnableFeignClients//使用feign访问其他微服务
+@EnableEurekaClient//eureka客户端
+@EnableHystrix
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+@EnableHystrixDashboard
+@SpringBootApplication
+public class SearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced//开启负载均衡
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
